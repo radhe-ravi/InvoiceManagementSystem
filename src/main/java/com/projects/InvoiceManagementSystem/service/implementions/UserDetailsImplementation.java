4fc278877a203b573d59be7c19c42725c050e603
RadheRavi3021 <radheravi01@gmail.com>
2024-06-21 13:02:49 +0530

    updated

--- src/main/java/com/projects/InvoiceManagementSystem/service/implementions/UserDetailsImplementation.java
+++ src/main/java/com/projects/InvoiceManagementSystem/service/implementions/UserDetailsImplementation.java
@@ -1,10 +1,11 @@
-package com.projects.InvoiceManagementSystem.Service.Implementions;
+package com.projects.InvoiceManagementSystem.service.implementions;
 
-import com.projects.InvoiceManagementSystem.DTO.UserDetailDTO;
-import com.projects.InvoiceManagementSystem.Entities.UserDetail;
-import com.projects.InvoiceManagementSystem.Repository.UserDetailRepository;
-import com.projects.InvoiceManagementSystem.Service.UserDetailsService;
+import com.projects.InvoiceManagementSystem.dto.UserDetailDto;
+import com.projects.InvoiceManagementSystem.entitiy.UserDetail;
+import com.projects.InvoiceManagementSystem.repository.UserDetailRepository;
+import com.projects.InvoiceManagementSystem.service.UserDetailsService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,37 +14,40 @@ import java.util.UUID;
 
 @AllArgsConstructor
 @Service
+@Slf4j
 public class UserDetailsImplementation implements UserDetailsService {
 
     private final UserDetailRepository userDetailRepository;
 
     @Override
-    public List<UserDetailDTO> getAllUsers() {
+    public List<UserDetailDto> getAllUsers() {
         return userDetailRepository.findAll().stream()
                 .map(this::convertToDTO).toList();
     }
 
     @Override
-    public UserDetailDTO getUserById(UUID userId) {
+    public UserDetailDto getUserById(UUID userId) {
         return userDetailRepository.findById(userId)
                 .map(this::convertToDTO)
                 .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
     @Override
-    public UserDetailDTO createUser(UserDetailDTO userDetailDTO) {
+    public UserDetailDto createUser(UserDetailDto userDetailDTO) {
+        log.info("Creating user: {}", userDetailDTO);
         UserDetail userDetail = new UserDetail();
         userDetail.setUserId(UUID.randomUUID());
         userDetail.setUserName(userDetailDTO.getUserName());
         userDetail.setCreatedOn(LocalDateTime.now());
         userDetail.setStatus(userDetailDTO.getStatus());
         userDetailRepository.save(userDetail);
+        log.info("User created: {}", convertToDTO(userDetail));
         return convertToDTO(userDetail);
     }
 
 
-    private UserDetailDTO convertToDTO(UserDetail userDetail) {
-        UserDetailDTO userDetailDTO = new UserDetailDTO();
+    private UserDetailDto convertToDTO(UserDetail userDetail) {
+        UserDetailDto userDetailDTO = new UserDetailDto();
         userDetailDTO.setUserId(userDetail.getUserId());
         userDetailDTO.setUserName(userDetail.getUserName());
         userDetailDTO.setStatus(userDetail.getStatus());
